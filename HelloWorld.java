f444afb6e1a67bb96d856e021a78a8b3046c7282
virajbadve03@gmail.com <virajbadve03@gmail.com>
2026-02-27 18:19:21 +0530

    Day 1 - Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Day 1 Complete!");
+        System.out.println("I am going to be a Spring Boot developer.");
+    }
+}
