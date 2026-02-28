a7a651162cf8320bcf9efa6fb5f930129bfecfe7
virajbadve03@gmail.com <virajbadve03@gmail.com>
2026-02-28 15:42:42 +0530

    Day 2 - Variables, Data Types, Operators

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,3 @@ public class HelloWorld{
         System.out.println("I am going to be a Spring Boot developer.");
     }
 }
-
-//git remote set-url origin https://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@github.com/Viiiraj-Dev/springboot-journey.git
